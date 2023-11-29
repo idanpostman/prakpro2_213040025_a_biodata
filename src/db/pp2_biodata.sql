0a55d46be552e92866c9b714db9a1261f89c8f62
idanpostman <idanpostman@gmail.com>
2023-11-29 08:48:18 +0700

    Menambahkan file sql

---
+++ src/db/pp2_biodata.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 29, 2023 at 01:39 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pp2_biodata`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `biodata`
+--
+
+CREATE TABLE `biodata` (
+  `id` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `no_telepon` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `biodata`
+--
+
+INSERT INTO `biodata` (`id`, `nama`, `no_telepon`, `jenis_kelamin`, `alamat`) VALUES
+('ec1cdaa8-3674-4340-8254-4da78376045a', 'idan', '085155222234', 'Laki-Laki', 'Sayati Indah No 14');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `biodata`
+--
+ALTER TABLE `biodata`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
