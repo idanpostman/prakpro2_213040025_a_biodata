7ee0c5258847d2c1d051f8699a77280f84e51084
idanpostman <idanpostman@gmail.com>
2023-11-29 08:26:13 +0700

    Menyelesaikan Studi Kasus 2 - Pertemuan 9

---
+++ src/main/Main.java
@@ -0,0 +1,44 @@
+package main;
+
+import javax.swing.*;
+import dao.BiodataDao;
+import biodata.BiodataFrame;
+
+/**
+ *
+ * @author acer
+ */
+
+// Class Main untuk menjalankan program
+public class Main extends JFrame {
+    private final BiodataDao biodataDao; // Variable biodataDao untuk menyimpan nilai biodataDao
+    private final BiodataFrame biodataFrame; // Variable biodataFrame untuk menyimpan nilai biodataFrame
+
+    // Constructor Main
+    public Main() {
+        // Set judul dari frame
+        this.setTitle("Biodata");
+
+        // Set ukuran dari frame
+        this.setSize(400, 500);
+        
+        // Inisialisasi nilai dari biodataDao dan biodataFrame
+        this.biodataDao = new BiodataDao();
+        this.biodataFrame = new BiodataFrame(biodataDao);
+
+        // Set frame agar tidak dapat di resize
+        this.biodataFrame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        // Method invokeLater dari SwingUtilities untuk menjalankan objek Runnable yang
+        // berguna menangani event dan update UI
+        SwingUtilities.invokeLater(new Runnable() {
+            // Override method run dari Runnable
+            public void run() {
+                // Instansiasi StudiKasus dengan nama h
+                new Main();
+            }
+        });
+    }
+}
